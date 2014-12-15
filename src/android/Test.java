210e68ea86eb9e46395d8b619262bb8708b90abd
Diwakar Shukla <diwakarshukla14@gmail.com>
2014-12-15 19:12:38 +0530

    fix final static variable START declaration error

--- src/android/Test.java
+++ src/android/Test.java
@@ -9,10 +9,12 @@ import org.json.JSONException;
 import android.widget.Toast;
 
 public class Test extends CordovaPlugin {
+	public static final String START = "start"; 
 	
+
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		 public static final String START = "start"; 
+		 
 		try{
 			Toast.makeText(this.cordova.getActivity(), "Background location tracking stopped", Toast.LENGTH_SHORT).show();
 	 	 	callbackContext.success();
