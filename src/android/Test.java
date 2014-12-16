9403f683be73fc0526ce9b680f81365bd2f08cba
Diwakar Shukla <diwakarshukla14@gmail.com>
2014-12-16 11:56:36 +0530

    turnGPSOn method added

--- src/android/Test.java
+++ src/android/Test.java
@@ -10,26 +10,39 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.provider.Settings;
+import android.net.Uri;
 import android.widget.Toast;
 
 public class Test extends CordovaPlugin {
 	public static final String START = "start"; 
-	
+	Context context=null;
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		Context context=this.cordova.getActivity().getApplicationContext();
+		context=this.cordova.getActivity().getApplicationContext();
 		try{
 			Toast.makeText(this.cordova.getActivity(), "Background location tracking stopped", Toast.LENGTH_SHORT).show();
-			final LocationManager manager = (LocationManager) context.getSystemService( Context.LOCATION_SERVICE );
-			if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
-			     Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-			     context.startActivity(intent);
-			}
+			turnGPSOn();
 	 	 	callbackContext.success();
 	 	 	return true;
 		}catch(Exception e){
 			return false;
 		}
+	}
+
+	public void turnGPSOn()
+	{
+	     Intent intent = new Intent("android.location.GPS_ENABLED_CHANGE");
+	     intent.putExtra("enabled", true);
+	     context.sendBroadcast(intent);
+
+	    String provider = Settings.Secure.getString(ctx.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+	    if(!provider.contains("gps")){ //if gps is disabled
+	        final Intent poke = new Intent();
+	        poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider"); 
+	        poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+	        poke.setData(Uri.parse("3")); 
+	        context.sendBroadcast(poke);
+	    }
 	}
 }
